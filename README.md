139c87b2078bb6b3dc302c2549bfdd2da2a954e9
nguyenduytan1996 <nguyenduytanzaroc@gmail.com>
2019-06-17 18:44:54 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-1x1pwj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-1x1pwj)
---
+++ src/app/dasd/dasd.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dasd',
+  templateUrl: './dasd.component.html',
+  styleUrls: ['./dasd.component.css']
+})
+export class DasdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
